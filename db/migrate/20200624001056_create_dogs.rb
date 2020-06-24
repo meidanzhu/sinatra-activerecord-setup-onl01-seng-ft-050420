83a3a8f27a815330a733f9404500d35b3348dc99
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-24 00:16:41 +0000

    Automatically backed up by Learn

--- db/migrate/20200624001056_create_dogs.rb
+++ db/migrate/20200624001056_create_dogs.rb
@@ -9,4 +9,12 @@ class CreateDogs < ActiveRecord::Migration[5.2]
   def down
     drop_table :dogs
   end
+
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+ end
+
 end
